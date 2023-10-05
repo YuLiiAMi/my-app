7fb113f85d251d1ad0b8af9cd8c3a502ec4ee589
Yuliia Miroshnichenko <tsegel1986@gmail.com>
2023-10-06 00:39:56 +0300

    add homework 2

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Login from "./containers/Login/Login";
+import ProductTable from "./containers/ProductTable/ProductTable";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Login />
+    <ProductTable />
+    {/* <Login /> */}
   </React.StrictMode>
 );
