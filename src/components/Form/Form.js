b866001f7d4a659ef5d417ee119448eed4911818
Yuliia Miroshnichenko <tsegel1986@gmail.com>
2023-10-10 13:14:48 +0300

    add form

---
+++ src/components/Form/Form.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import "./Form.css";
+import logo from "../../logo.svg";
+import Input from "../../components/Input/Input";
+import Button from "../../components/Button/Button";
+
+const Form = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({ userName: "", password: "" });
+  const [message, setMessage] = useState("");
+
+  const validateForm = () => {
+    let isValid = true;
+    const newErrors = { userName: "", password: "" };
+
+    if (!userName) {
+      newErrors.userName = "Введите логин";
+      isValid = false;
+    }
+
+    if (!password) {
+      newErrors.password = "Введите пароль";
+      isValid = false;
+    }
+
+    setErrors(newErrors);
+    return isValid;
+  };
+
+  const handleSubmit = async () => {
+    if (validateForm()) {
+      try {
+        const response = await fetch("http://localhost:5000/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username: userName, password: password }),
+        });
+
+        if (response.status === 200) {
+          const data = await response.json();
+          localStorage.setItem("token", data.token);
+          setUserName("");
+          setPassword("");
+          setMessage("Ваши данные успешно отправлены");
+        } else {
+          setUserName("");
+          setPassword("");
+          const errorMessage = await response.text();
+          setMessage(`${errorMessage}`);
+        }
+      } catch (error) {
+        console.error("Ошибка во время авторизации", error);
+      }
+    }
+  };
+
+  return (
+    <div className="form-container">
+      <img src={logo} className="App-logo" alt="logo" />
+      <Input
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+        placeholder="User name"
+        error={errors.userName}
+      />
+      <Input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+        error={errors.password}
+      />
+      <Button onClick={handleSubmit} text="Login" />
+      {message && <div className="message">{message}</div>}{" "}
+    </div>
+  );
+};
+
+export default Form;
--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,12 +1,41 @@
+import React, { useState } from "react";
 import "./Input.css";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 
-const Input = () => {
+const Input = ({ type = "text", placeholder, value, onChange }) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleBlur = () => {
+    if (value === "") {
+      setError(`Введите ${placeholder.toLowerCase()}`);
+    } else {
+      setError("");
+    }
+  };
+
+  const handleChange = (e) => {
+    if (e.target.value.length > 0) {
+      setError("");
+    }
+    onChange(e);
+  };
+
   return (
-    <div className="login-input">
-      <input type="text" placeholder="User name" />
-      <input type="text" placeholder="Password" />
-      <FaEye className="eyeIcon" />
+    <div className={`login-input ${error ? "error" : ""}`}>
+      <input
+        type={showPassword ? "text" : type}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        onBlur={handleBlur}
+      />
+      {type === "password" && (
+        <div className="eyeIcon" onClick={() => setShowPassword(!showPassword)}>
+          {showPassword ? <FaEyeSlash /> : <FaEye />}
+        </div>
+      )}
+      {error && <div className="error-message">{error}</div>}
     </div>
   );
 };
